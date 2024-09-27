19509d3b32cffd1e69b961deffe139a6ed2c280f
Falme Streamless <falme_streamless@hotmail.com>
2024-09-27 20:39:49 -0300

    Modificado Todos os Paragrafos em Arrecadar Dinheiro

--- includes/arrecadar-dinheiro.php
+++ includes/arrecadar-dinheiro.php
@@ -4,84 +4,99 @@
     <img src="assets/images/gamedev_splash.png" style="width: 100%" /> <br />
     <h1>Como faço para arrecadar dinheiro?</h1>
     <div class="text-separator"></div>
-    <br /><br />
-    Ganhar dinheiro como desenvolvedor independente pode ser uma tarefa realmente difícil, especialmente quando você está apenas começando. O conselho que sempre darei é não abandonar seu trabalho diário. Faça jogos como um hobby no início. Volte para casa todos os dias do trabalho e passe algumas horas trabalhando no seu jogo. Faça disso uma rotina. Economize dinheiro a cada cheque de pagamento em seu fundo independente para ajudar a obter ativos e software para o seu hobby. Mantenha as coisas pequenas e isso é absolutamente viável. <br /><br />
-    Existem várias maneiras de arrecadar fundos externos para seus jogos. Vou listar alguns deles abaixo e os pontos fortes e fracos de cada um. Cada plataforma tem seus benefícios e cada plataforma tem seus pontos fracos. Espalhar seu jogo para mais plataformas dificulta o gerenciamento, mas também pode ampliar seu alcance. É tudo uma questão de equilíbrio e impacto, e esse processo será diferente para cada jogo. Vá devagar, descubra as coisas, mova-se quando parecer certo e você saberá que pode fazer isso. <br /><br />
-    Você também deve ficar atento às pessoas que estão tentando ganhar um pedaço do seu bolo de graça. Desenvolvedores recebem toneladas de ofertas fraudulentas todos os dias de todos os tipos de fontes. Se você acidentalmente pisar em qualquer uma dessas minas terrestres, poderá ser a morte do seu projeto sem que você perceba até meses ou até anos depois. Não se venda por pouco e fique atento a algumas das armadilhas abaixo. Esse é o seu bolo e você precisa protegê-lo. <br /><br />
-    Com tudo isso dito, aqui estão alguns métodos para ser usado para gerar dinheiro e ainda manter a máquina rodando. <br /><br /><br />
+    <p>
+        Ganhar dinheiro como desenvolvedor independente pode ser uma tarefa realmente difícil, especialmente quando você está apenas começando. O conselho que sempre darei é não abandonar seu trabalho diário. Faça jogos como um hobby no início. Volte para casa todos os dias do trabalho e passe algumas horas trabalhando no seu jogo. Faça disso uma rotina. Economize dinheiro a cada cheque de pagamento em seu fundo independente para ajudar a obter ativos e software para o seu hobby. Mantenha as coisas pequenas e isso é absolutamente viável.
+    </p>
+    <p>
+        Existem várias maneiras de arrecadar fundos externos para seus jogos. Vou listar alguns deles abaixo e os pontos fortes e fracos de cada um. Cada plataforma tem seus benefícios e cada plataforma tem seus pontos fracos. Espalhar seu jogo para mais plataformas dificulta o gerenciamento, mas também pode ampliar seu alcance. É tudo uma questão de equilíbrio e impacto, e esse processo será diferente para cada jogo. Vá devagar, descubra as coisas, mova-se quando parecer certo e você saberá que pode fazer isso.
+    </p>
+    <p>
+        Você também deve ficar atento às pessoas que estão tentando ganhar um pedaço do seu bolo de graça. Desenvolvedores recebem toneladas de ofertas fraudulentas todos os dias de todos os tipos de fontes. Se você acidentalmente pisar em qualquer uma dessas minas terrestres, poderá ser a morte do seu projeto sem que você perceba até meses ou até anos depois. Não se venda por pouco e fique atento a algumas das armadilhas abaixo. Esse é o seu bolo e você precisa protegê-lo.
+    </p>
+    <p>
+        Com tudo isso dito, aqui estão alguns métodos para ser usado para gerar dinheiro e ainda manter a máquina rodando.
+    </p>
     <div id="nav-finance-stream"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Streaming</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    <img src="assets/images/twitch_splash.jpg" /> <br /><br />
-    <a href="https://www.twitch.tv/" target="_blank" rel="noopener noreferrer">Twitch</a> Tem sido a base da receitas de estúdios. Serve como uma ótima maneira de interagir com a comunidade, uma fonte de receita e uma propaganda interativa em tempo real do trabalho que realizamos. <br /><br />
-    Depois de uma <a href="https://twitch.uservoice.com/forums/310210-discover-browsing-and-searching/suggestions/42795278-create-a-game-development-category" target="_blank" rel="noopener noreferrer" >longa caminhada</a> Twitch recentemente criou a categoria de <a href="https://www.twitch.tv/directory/game/Software%20and%20Game%20Development" target="_blank" rel="noopener noreferrer" >Desenvolvimento de Programas e Jogos</a>. Finalmente temos um lar como desenvolvedores de jogos e não poderiamos estar mais feliz. <br /><br /><br />
+    <img src="assets/images/twitch_splash.jpg" /> <br />
+    <p>
+        <a href="https://www.twitch.tv/" target="_blank" rel="noopener noreferrer">Twitch</a> Tem sido a base da receitas de estúdios. Serve como uma ótima maneira de interagir com a comunidade, uma fonte de receita e uma propaganda interativa em tempo real do trabalho que realizamos.
+    </p>
+    <p>
+        Depois de uma <a href="https://twitch.uservoice.com/forums/310210-discover-browsing-and-searching/suggestions/42795278-create-a-game-development-category" target="_blank" rel="noopener noreferrer" >longa caminhada</a> Twitch recentemente criou a categoria de <a href="https://www.twitch.tv/directory/game/Software%20and%20Game%20Development" target="_blank" rel="noopener noreferrer" >Desenvolvimento de Programas e Jogos</a>. Finalmente temos um lar como desenvolvedores de jogos e não poderiamos estar mais feliz.
+    </p>
     <div id="nav-finance-publishers"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Publicadoras</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    <img src="assets/images/publisher_splash.jpg" /> <br /><br />
-    Sempre que estiver pensando em contratar uma publicadora, tenha apenas uma pergunta em mente: Será que a ajuda da publicadora conduzirá a lucros superiores e além ao corte de receitas? Se você não sabe a resposta ou se a resposta for não, então você não precisa de uma publicadora. Se você pode lançar seu jogo sem uma publicadora, então você absolutamente deveria não contratar uma publicadora. <br /><br />
-    No geral, maioria das publicadoras pedem por 30%-70% da receita do seu jogo. Esse custo vem carregado de uma época triássica, dos dias onde as publicadoras manuseavam cartuchos físicos nas prateleiras das lojas de varejo. Não precisamos mais disso devido à distribuição digital como Steam, Humble Bundle, etc. <br /><br /><br />
+    <img src="assets/images/publisher_splash.jpg" /> <br />
+    <p>
+        Sempre que estiver pensando em contratar uma publicadora, tenha apenas uma pergunta em mente: Será que a ajuda da publicadora conduzirá a lucros superiores e além ao corte de receitas? Se você não sabe a resposta ou se a resposta for não, então você não precisa de uma publicadora. Se você pode lançar seu jogo sem uma publicadora, então você absolutamente deveria não contratar uma publicadora.
+    </p>
+    <p>
+        No geral, maioria das publicadoras pedem por 30%-70% da receita do seu jogo. Esse custo vem carregado de uma época triássica, dos dias onde as publicadoras manuseavam cartuchos físicos nas prateleiras das lojas de varejo. Não precisamos mais disso devido à distribuição digital como Steam, Humble Bundle, etc.
+    </p>
     <div id="nav-finance-kick"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Catarse</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    <img src="assets/images/catarse.jpg" /> <br /><br />
-    <a href="https://www.catarse.me/" target="_blank" rel="noopener noreferrer">Catarse</a> é um ótimo lugar para arrecadar fundos quando você já tem uma base da comunidade e uma demonstração para apresentar. Eu não recomendaria seguir esse caminho antes de ter essas coisas prontas. Eu também não confiaria no Catarse como algo que financiará todo o desenvolvimento do seu jogo. Normalmente é usado como plataforma de marketing e para obter financiamento inicial para hardware e software. Também deve-se pensar nas recompensas do público por te financiar, isso também é um custo que deve ser pensado e balanceado com o financeiro. Espere para lançar seu Catarse até saber que terá sucesso. Não deixe isso ao acaso. <br /><br /><br />
+    <img src="assets/images/catarse.jpg" /> <br />
+    <p>
+        <a href="https://www.catarse.me/" target="_blank" rel="noopener noreferrer">Catarse</a> é um ótimo lugar para arrecadar fundos quando você já tem uma base da comunidade e uma demonstração para apresentar. Eu não recomendaria seguir esse caminho antes de ter essas coisas prontas. Eu também não confiaria no Catarse como algo que financiará todo o desenvolvimento do seu jogo. Normalmente é usado como plataforma de marketing e para obter financiamento inicial para hardware e software. Também deve-se pensar nas recompensas do público por te financiar, isso também é um custo que deve ser pensado e balanceado com o financeiro. Espere para lançar seu Catarse até saber que terá sucesso. Não deixe isso ao acaso.
+    </p>
     <div id="nav-finance-subscription"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Serviços de assinatura</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    <img src="assets/images/kofi_splash.png" /> <br /><br />
-    <a href="https://ko-fi.com/" target="_blank" rel="noopener noreferrer" >Ko-Fi</a> é como Patreon se Patreon não fosse o verdadeiro diabo. Ao contrário do diabo real, o Ko-Fi cobra uma taxa fixa pelo seu serviço de assinatura. No momento são 54 dólares por ano. São 4,50 dólares por mês. Sem porcentagens ocultas ou outras malandragens. Literalmente todas as funcionalidades que o Patreon oferece, mas sem o imposto infernal de 8%. <br /><br />
-    Quando as pessoas ingressam nos "Tiers", elas também pagam imediatamente, para que não haja mais pagamentos de compensação ou conteúdo roubado quando alguém recusa seu cartão no final do mês. Eles também possuem um sistema de alerta para o seu stream e você pode aceitar doações por meio deles. Mude para Ko-Fi. Serio. <br /><br /><br />
+    <img src="assets/images/kofi_splash.png" /> <br />
+    <p>
+        <a href="https://ko-fi.com/" target="_blank" rel="noopener noreferrer" >Ko-Fi</a> é como Patreon se Patreon não fosse o verdadeiro diabo. Ao contrário do diabo real, o Ko-Fi cobra uma taxa fixa pelo seu serviço de assinatura. No momento são 54 dólares por ano. São 4,50 dólares por mês. Sem porcentagens ocultas ou outras malandragens. Literalmente todas as funcionalidades que o Patreon oferece, mas sem o imposto infernal de 8%.
+    </p>
+    <p>
+        Quando as pessoas ingressam nos "Tiers", elas também pagam imediatamente, para que não haja mais pagamentos de compensação ou conteúdo roubado quando alguém recusa seu cartão no final do mês. Eles também possuem um sistema de alerta para o seu stream e você pode aceitar doações por meio deles. Mude para Ko-Fi. Serio.
+    </p>
     <div id="nav-finance-scams"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Golpes</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    <img src="" style="width: 25%" /> <br /><br />
-    Os golpes são uma armadilha muito comum no mundo do desenvolvedor independente. Não assine nada que não tenha lido. Se você não tiver certeza do que algo significa em um contrato, converse com um advogado. Se você acidentalmente distribuir chaves para o seu jogo, poderá gravá-las por meio das ferramentas do Parceiro Steam, então não se preocupe. Fora isso, aqui estão alguns exemplos de alguns golpes que devem ser evitados. <br /><br /><br /><br />
+    <img src="" style="width: 25%" /> <br />
+    <p>
+        Os golpes são uma armadilha muito comum no mundo do desenvolvedor independente. Não assine nada que não tenha lido. Se você não tiver certeza do que algo significa em um contrato, converse com um advogado. Se você acidentalmente distribuir chaves para o seu jogo, poderá gravá-las por meio das ferramentas do Parceiro Steam, então não se preocupe. Fora isso, aqui estão alguns exemplos de alguns golpes que devem ser evitados.
+    </p>
     <div class="text-separator"></div>
-    <br />
     <h3>O golpe do influenciador</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Esta é a forma mais comum de golpe direcionado a desenvolvedores independentes. <br />
-    Você vai encontrar eles. <br />
-    todos nós encontramos. <br /><br />
-    O golpe geralmente funciona por e-mail e começa com alguém alegando ser um streamer ou jornalista de jogos. Eles usarão o ícone correto para seu perfil, mostrarão links legítimos para contas de mídia social e falarão um pouco sobre seu conteúdo. No entanto, o e-mail não estará correto. Sempre verifique o e-mail comercial de um influenciador antes de enviar as chaves. Geralmente você pode encontrar isso na seção "Sobre" do YouTube. Pode ser apenas uma letra errada, mas não é o verdadeiro criador entrando em contato com você se o email não corresponder. <br /><br />
-    Esses e-mails são automatizados e a chave acaba indo parar em um site de terceiros para ser vendida por um preço inferior ao preço do seu jogo. Dos milhares de e-mails, apenas um punhado dos emails são seres humanos legítimos. Esteja atento em suas verificações e não se surpreenda com um criador potencialmente famoso enviando um e-mail para você. <br /><br />
-    Com isso em mente, minha maneira de lidar com isso mudou um pouco nos últimos anos. Agora dou a eles a Steam Key e a gravo 12 a 24 horas depois. Do lado dos golpistas, seu sistema automatizado verifica a chave e a coloca à venda. Aí alguém compra e não funciona. É provável que esse cliente deixe uma avaliação negativa para o vendedor fraudulento. Acerte-os onde dói. Em sua reputação. Depois de iniciar essa tática, meus e-mails fraudulentos de influenciadores diminuíram cerca de 90% e não voltaram. Acho que eles têm uma lista negra. <br /><br /><br /><br />
+    <p>
+        Esta é a forma mais comum de golpe direcionado a desenvolvedores independentes.
+    </p>
+    <p>
+        Você vai encontrar eles.
+    </p>
+    <p>
+        todos nós encontramos.
+    </p>
+    <p>
+        O golpe geralmente funciona por e-mail e começa com alguém alegando ser um streamer ou jornalista de jogos. Eles usarão o ícone correto para seu perfil, mostrarão links legítimos para contas de mídia social e falarão um pouco sobre seu conteúdo. No entanto, o e-mail não estará correto. Sempre verifique o e-mail comercial de um influenciador antes de enviar as chaves. Geralmente você pode encontrar isso na seção "Sobre" do YouTube. Pode ser apenas uma letra errada, mas não é o verdadeiro criador entrando em contato com você se o email não corresponder.
+    </p>
+    <p>
+        Esses e-mails são automatizados e a chave acaba indo parar em um site de terceiros para ser vendida por um preço inferior ao preço do seu jogo. Dos milhares de e-mails, apenas um punhado dos emails são seres humanos legítimos. Esteja atento em suas verificações e não se surpreenda com um criador potencialmente famoso enviando um e-mail para você.
+    </p>
+    <p>
+        Com isso em mente, minha maneira de lidar com isso mudou um pouco nos últimos anos. Agora dou a eles a Steam Key e a gravo 12 a 24 horas depois. Do lado dos golpistas, seu sistema automatizado verifica a chave e a coloca à venda. Aí alguém compra e não funciona. É provável que esse cliente deixe uma avaliação negativa para o vendedor fraudulento. Acerte-os onde dói. Em sua reputação. Depois de iniciar essa tática, meus e-mails fraudulentos de influenciadores diminuíram cerca de 90% e não voltaram. Acho que eles têm uma lista negra.
+    </p>
     <div class="text-separator"></div>
-    <br />
     <h3>O golpe por vendas em pacote</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Muitas vezes você receberá ofertas para fazer parte de um pacote. Os golpistas alegarão que você receberá entre 10-30% das vendas do pacote se lhes der 10.000-30.000 chaves Steam para o seu jogo. Se você assinar o contrato e enviar as chaves, eles começarão a vendê-las normalmente até certo ponto. A partir daqui eles iniciarão um “Happy Hour” e venderão de 5 a 10 vezes mais chaves por compra. Outra empresa sabe quando começa esse Happy Hour e vai comprar todas as chaves nos próximos minutos. <br /><br />
-    Você fica então com 10-20% do valor inicial que acreditava estar recebendo. Ao mesmo tempo, outra empresa agora possui dezenas de milhares de chaves de jogos e reduzirá os preços do Steam em todos os sites de chaves de terceiros na rede. Isso terá um impacto considerável na sua capacidade de vender cópias do seu jogo. <br /><br />
-    Esses golpes são raros, bem pensados ​​e parecem lógicos do ponto de vista comercial. Fale com um representante jurídico e certifique-se de saber no que está se metendo antes de assinar qualquer coisa. Não se deixe levar por números de vendas brilhantes.
+    <p>
+        Muitas vezes você receberá ofertas para fazer parte de um pacote. Os golpistas alegarão que você receberá entre 10-30% das vendas do pacote se lhes der 10.000-30.000 chaves Steam para o seu jogo. Se você assinar o contrato e enviar as chaves, eles começarão a vendê-las normalmente até certo ponto. A partir daqui eles iniciarão um “Happy Hour” e venderão de 5 a 10 vezes mais chaves por compra. Outra empresa sabe quando começa esse Happy Hour e vai comprar todas as chaves nos próximos minutos.
+    </p>
+    <p>
+        Você fica então com 10-20% do valor inicial que acreditava estar recebendo. Ao mesmo tempo, outra empresa agora possui dezenas de milhares de chaves de jogos e reduzirá os preços do Steam em todos os sites de chaves de terceiros na rede. Isso terá um impacto considerável na sua capacidade de vender cópias do seu jogo.
+    </p>
+    <p>
+        Esses golpes são raros, bem pensados ​​e parecem lógicos do ponto de vista comercial. Fale com um representante jurídico e certifique-se de saber no que está se metendo antes de assinar qualquer coisa. Não se deixe levar por números de vendas brilhantes.
+    </p>
 </div>
 <br /><br />
