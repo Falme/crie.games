5771cad76bce12a95f30bf2e9d96d3ee637db8b0
Falme Streamless <falme_streamless@hotmail.com>
2024-09-27 22:19:21 -0300

    Modificado Todos os Paragrafos em Publicar

--- includes/publicar.php
+++ includes/publicar.php
@@ -4,33 +4,40 @@
 	<img src="assets/images/gamedev_splash.png" style="width: 100%" /> <br />
 	<h1>Onde eu deveria lançar meu jogo?</h1>
 	<div class="text-separator"></div>
-	<br /><br />
-	Sim. Não, sério... sim. Existem tantos lugares diferentes onde você pode lançar seus jogos hoje. Adotamos uma abordagem muito centrada no Steam porque ela facilita muito o trabalho com terceiros. Isso nos permitiu ampliar imensamente nosso alcance sem aumentar muito nossa carga de trabalho total. Como uma equipe pequena, isso é enorme e funcionou bem para nós. <br /><br /><br />
+	<p>
+		Sim. Não, sério... sim. Existem tantos lugares diferentes onde você pode lançar seus jogos hoje. Adotamos uma abordagem muito centrada no Steam porque ela facilita muito o trabalho com terceiros. Isso nos permitiu ampliar imensamente nosso alcance sem aumentar muito nossa carga de trabalho total. Como uma equipe pequena, isso é enorme e funcionou bem para nós.
+	</p>
 	<div class="text-separator"></div>
-	<br />
 	<div id="nav-launch-steam"></div>
-	<br />
-	<img src="assets/images/steam_splash.jpg" /> <br /><br />
-	<a href="https://store.steampowered.com/" target="_blank" rel="noopener noreferrer" >Steam</a> é a melhor plataforma de monetização que existe para jogos. Com um front-end enorme voltado para a interação e feedback dos jogadores, um back-end robusto para ajudá-lo a gerenciar seus ativos e finanças e toneladas de ferramentas promocionais, o Steam realmente tem tudo. <br /><br />
-	O Steam tem vendas regulares em toda a plataforma nas quais você pode participar. Você pode definir o preço de venda e pode aceitar ou recusar à vontade. Eles não fazem nada com o seu jogo sem o seu consentimento em nenhum momento. Adicionar seu jogo a uma promoção geralmente dá um grande impulso à descoberta durante a venda. Se você colocar seu jogo à venda com pelo menos 20% de desconto ou mais, todos que colocaram o jogo na lista de desejos receberão um e-mail sobre isso. Use isso para ajudar a aumentar a visibilidade e colocar seu jogo nas mãos de mais pessoas. <br /><br />
-	Para cada jogo iniciado, você receberá cinco 'Rodadas de Visibilidade'. Esses VRs dão ao seu jogo um tempo de primeira página da ordem de 1.000.000 de impressões. Como um pequeno estúdio, este é um grande negócio e só deve ser usado para atualizações massivas. Esta é a sua hora de realmente mostrar seu jogo e obter uma grande quantidade de tráfego intenso. Você realmente precisa ter certeza de que tudo está sólido antes de disparar um desses. Além disso, se você iniciar seu jogo no Acesso Antecipado... depois de iniciar totalmente, você receberá imediatamente um VR gratuito que não contará nos cinco jogos. Lance no Acesso Antecipado, receba feedback, use-o para melhorar, lance totalmente e obtenha uma explosão gratuita de visibilidade. <br /><br />
-	O Steam fica com uma redução de 30% em todas as vendas, mas isso não conta toda a história. Eles permitem a localização regional de preços, o que aumenta ENORME a sua receita e também cobram 0% sempre que você vende uma chave Steam em uma plataforma de terceiros. Isso significa que você pode vender em qualquer lugar do mundo ou em outras plataformas com relativa facilidade e ao mesmo tempo gerenciar apenas uma única base de distribuição. Ao localizar nossos preços, nossa renda aumentou em 20-25% somente no Brasil. Localize seus jogos. Localize seus preços. O vapor é ótimo. Não deixe ninguém te convencer do contrário. <br /><br /><br />
+	<img src="assets/images/steam_splash.jpg" /> <br />
+	<p>
+		<a href="https://store.steampowered.com/" target="_blank" rel="noopener noreferrer" >Steam</a> é a melhor plataforma de monetização que existe para jogos. Com um front-end enorme voltado para a interação e feedback dos jogadores, um back-end robusto para ajudá-lo a gerenciar seus ativos e finanças e toneladas de ferramentas promocionais, o Steam realmente tem tudo.
+	</p>
+	<p>
+		O Steam tem vendas regulares em toda a plataforma nas quais você pode participar. Você pode definir o preço de venda e pode aceitar ou recusar à vontade. Eles não fazem nada com o seu jogo sem o seu consentimento em nenhum momento. Adicionar seu jogo a uma promoção geralmente dá um grande impulso à descoberta durante a venda. Se você colocar seu jogo à venda com pelo menos 20% de desconto ou mais, todos que colocaram o jogo na lista de desejos receberão um e-mail sobre isso. Use isso para ajudar a aumentar a visibilidade e colocar seu jogo nas mãos de mais pessoas.
+	</p>
+	<p>
+		Para cada jogo iniciado, você receberá cinco 'Rodadas de Visibilidade'. Esses VRs dão ao seu jogo um tempo de primeira página da ordem de 1.000.000 de impressões. Como um pequeno estúdio, este é um grande negócio e só deve ser usado para atualizações massivas. Esta é a sua hora de realmente mostrar seu jogo e obter uma grande quantidade de tráfego intenso. Você realmente precisa ter certeza de que tudo está sólido antes de disparar um desses. Além disso, se você iniciar seu jogo no Acesso Antecipado... depois de iniciar totalmente, você receberá imediatamente um VR gratuito que não contará nos cinco jogos. Lance no Acesso Antecipado, receba feedback, use-o para melhorar, lance totalmente e obtenha uma explosão gratuita de visibilidade.
+	</p>
+	<p>
+		O Steam fica com uma redução de 30% em todas as vendas, mas isso não conta toda a história. Eles permitem a localização regional de preços, o que aumenta ENORME a sua receita e também cobram 0% sempre que você vende uma chave Steam em uma plataforma de terceiros. Isso significa que você pode vender em qualquer lugar do mundo ou em outras plataformas com relativa facilidade e ao mesmo tempo gerenciar apenas uma única base de distribuição. Ao localizar nossos preços, nossa renda aumentou em 20-25% somente no Brasil. Localize seus jogos. Localize seus preços. O vapor é ótimo. Não deixe ninguém te convencer do contrário.
+	</p>
 	<div class="text-separator"></div>
-	<br />
 	<div id="nav-launch-humble"></div>
-	<br />
-	<img src="assets/images/humble_splash.webp" /> <br /><br />
-	<a href="https://www.humblebundle.com/" target="_blank" rel="noopener noreferrer" >Humble Bundle</a> é uma ótima plataforma intermediária que pode ajudar a melhorar suas vendas distribuindo chaves Steam. Eles não recebem nenhum valor de sua venda, a menos que você doe parte de seu lucro para eles, o que pode realmente ajudar quando você está apenas começando. Eles também têm um programa de parceria para que as pessoas que compram coisas que você recomenda possam lhe dar um pequeno aumento na renda de forma passiva. Geralmente coloco links de parceria para ferramentas e jogos que usamos ou jogamos em streaming. <br /><br /><br />
+	<img src="assets/images/humble_splash.webp" /> <br />
+	<p>
+		<a href="https://www.humblebundle.com/" target="_blank" rel="noopener noreferrer" >Humble Bundle</a> é uma ótima plataforma intermediária que pode ajudar a melhorar suas vendas distribuindo chaves Steam. Eles não recebem nenhum valor de sua venda, a menos que você doe parte de seu lucro para eles, o que pode realmente ajudar quando você está apenas começando. Eles também têm um programa de parceria para que as pessoas que compram coisas que você recomenda possam lhe dar um pequeno aumento na renda de forma passiva. Geralmente coloco links de parceria para ferramentas e jogos que usamos ou jogamos em streaming.
+	</p>
 	<div class="text-separator"></div>
-	<br />
 	<div id="nav-launch-jolt"></div>
-	<br />
-	<img src="assets/images/gamejolt_splash.jpg" /> <br /><br />
-	<a href="https://gamejolt.com/" target="_blank" rel="noopener noreferrer" >Game Jolt</a> é o deus dos sites de demonstração. Faça uma demonstração do seu jogo e coloque no Game Jolt. Quando lançaram uma demonstração de Heartbound no Steam pela primeira vez, viram um tráfego mínimo. Assim que colocaram no Game Jolt, milhares de pessoas o jogaram e, no primeiro ano, quase 95% de todos os vídeos do YouTube para Heartbound listaram o Game Jolt como o local de download. Foi muito importante para eles e realmente ajudou a divulgar o jogo. Eles também oferecem um pouco de dinheiro extra com os anúncios exibidos na sua página de jogos. Eles não precisam fazer isso e é uma coisa muito boa para lucrar de vez em quando. <br /><br /><br />
+	<img src="assets/images/gamejolt_splash.jpg" /> <br />
+	<p>
+		<a href="https://gamejolt.com/" target="_blank" rel="noopener noreferrer" >Game Jolt</a> é o deus dos sites de demonstração. Faça uma demonstração do seu jogo e coloque no Game Jolt. Quando lançaram uma demonstração de Heartbound no Steam pela primeira vez, viram um tráfego mínimo. Assim que colocaram no Game Jolt, milhares de pessoas o jogaram e, no primeiro ano, quase 95% de todos os vídeos do YouTube para Heartbound listaram o Game Jolt como o local de download. Foi muito importante para eles e realmente ajudou a divulgar o jogo. Eles também oferecem um pouco de dinheiro extra com os anúncios exibidos na sua página de jogos. Eles não precisam fazer isso e é uma coisa muito boa para lucrar de vez em quando.
+	</p>
 	<div class="text-separator"></div>
-	<br />
 	<div id="nav-launch-itch"></div>
-	<br />
-	<img src="assets/images/itch_splash.jpg" /> <br /><br />
-	<a href="https://itch.io/" target="_blank" rel="noopener noreferrer" >Itch.io</a> tem mais capacidade de personalização do que qualquer outro site, mas infelizmente carece no departamento de descoberta. Você definitivamente levará a maioria das pessoas a encontrar sua página aqui, mas quando o fizerem... ah, minha ciência... será lindo. Quer dizer... basta olhar o nosso no link aí. Você também pode vender chaves Steam através do Itch.io como mais uma fonte de renda com grande efeito. Se você deseja uma vitrine sofisticada e bonita para realizar suas vendas, este é o site para você. <br /><br />
+	<img src="assets/images/itch_splash.jpg" /> <br />
+	<p>
+		<a href="https://itch.io/" target="_blank" rel="noopener noreferrer" >Itch.io</a> tem mais capacidade de personalização do que qualquer outro site, mas infelizmente carece no departamento de descoberta. Você definitivamente levará a maioria das pessoas a encontrar sua página aqui, mas quando o fizerem... ah, minha ciência... será lindo. Quer dizer... basta olhar o nosso no link aí. Você também pode vender chaves Steam através do Itch.io como mais uma fonte de renda com grande efeito. Se você deseja uma vitrine sofisticada e bonita para realizar suas vendas, este é o site para você.
+	</p>
 </div>
