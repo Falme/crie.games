f7ada634870f8460b590f17978bedf5d672db761
Falme Streamless <falme_streamless@hotmail.com>
2024-09-27 22:16:39 -0300

    Modificado Todos os Paragrafos em Marketing

--- includes/marketing.php
+++ includes/marketing.php
@@ -4,61 +4,70 @@
     <img src="assets/images/gamedev_splash.png" style="width: 100%" />
     <h1>Marketing</h1>
     <div class="text-separator"></div>
-    <br />
-    Existem várias maneiras diferentes de abordar o marketing para desenvolvedores independentes. A principal coisa a lembrar sempre é que se alguém disser que existe uma maneira melhor, provavelmente está vendendo algo para você e isso geralmente só dá mais dinheiro para esse alguém. Faça o que achar melhor para seu estúdio, seus jogos e sua comunidade. Sempre. <br /><br />
+    <p>
+        Existem várias maneiras diferentes de abordar o marketing para desenvolvedores independentes. A principal coisa a lembrar sempre é que se alguém disser que existe uma maneira melhor, provavelmente está vendendo algo para você e isso geralmente só dá mais dinheiro para esse alguém. Faça o que achar melhor para seu estúdio, seus jogos e sua comunidade. Sempre.
+    </p>
     <div id="nav-marketing-community"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Construindo uma comunidade</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Construir uma comunidade é o mais importante em tudo isso. Sem a sua comunidade você não é absolutamente nada, não importa o quão incrível seja o seu jogo. Esta regra é válida independentemente do tamanho ou do financiamento do seu estúdio. Se você perder a fé dos seus jogadores, você perderá tudo. <br /><br />
-    Você nunca está acima de sua comunidade e nunca é importante demais para perder tempo respondendo a eles. No momento em que você pensa que é, eles lhe mostrarão o quão falso isso realmente é. Não se esqueça disso. <br /><br />
+    <p>
+        Construir uma comunidade é o mais importante em tudo isso. Sem a sua comunidade você não é absolutamente nada, não importa o quão incrível seja o seu jogo. Esta regra é válida independentemente do tamanho ou do financiamento do seu estúdio. Se você perder a fé dos seus jogadores, você perderá tudo.
+    </p>
+    <p>
+        Você nunca está acima de sua comunidade e nunca é importante demais para perder tempo respondendo a eles. No momento em que você pensa que é, eles lhe mostrarão o quão falso isso realmente é. Não se esqueça disso.
+    </p>
     <div id="nav-marketing-socialmedia"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Redes Sociais</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Existem vários lugares online onde você pode postar sobre seus jogos ou o que quer que esteja fazendo. Nunca perca uma oportunidade de fazer isso. Você é um desenvolvedor independente e precisa ficar no topo da montanha e gritar sobre isso. Você não é um Triple A com milhões de dólares para um orçamento de marketing, então aproveite todas as oportunidades que puder para mostrar seu trabalho às pessoas. Seu trabalho não precisa ser perfeito para falar sobre isso, então divulgue-o! <br /><br />
-    A forma como iniciamos esse processo foi configurando contas sociais para ajudar a reunir todas as pessoas em um só lugar. Estabelecemos o Discord como o principal centro comunitário que permitiu às pessoas um lugar comum para se encontrarem e se misturarem. Em seguida, configuramos contas satélite para direcionar as pessoas de volta ao Discord. Isso inclui coisas como <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" >Twitter</a>, <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" >Instagram</a>, <a href="https://store.steampowered.com/" target="_blank" rel="noopener noreferrer" >Steam</a>, <a href="https://gamejolt.com/" target="_blank" rel="noopener noreferrer" >Game Jolt</a>, <a href="https://itch.io/" target="_blank" rel="noopener noreferrer" >Itch.io</a>, <a href="https://twitch.tv" target="_blank" rel="noopener noreferrer" >Twitch</a>, <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" >YouTube</a>, <a href="https://www.reddit.com/" target="_blank" rel="noopener noreferrer" >Reddit</a>, e <a href="https://guilded.gg/" target="_blank" rel="noopener noreferrer" >Guilded</a>. Esse efeito de funil aumentou nosso Discord para mais de 4.000 membros e mantém todos na mesma página para que as atualizações não sejam perdidas pelos jogadores mais engajados. <br /><br />
-    As melhores e mais ativas plataformas de mídia social mudarão com o tempo, com base nos caprichos da Internet. É muito importante estar à frente dessas mudanças ou pelo menos acompanhá-las para não ser pego de surpresa. Eu rotineiramente visito vários sites e crio contas para o Pirate Software para ter certeza de que estamos preparados para o futuro. <br /><br />
-    É importante ver a sua presença nas redes sociais como algo muito pessoal para os seus jogadores. Simplesmente responder postagens aqui e ali, curtir ou entrar no stream de alguém quando ele joga seu jogo é enorme. Pode ser uma coisa simples para você, mas é absolutamente devastador para alguém que ama o seu trabalho. Faça isso. Comente todos os vídeos do YouTube sobre o seu jogo. Responda no Twitter sempre que alguém mencionar seu jogo de forma positiva e converse com as pessoas que responderam negativamente sobre como você pode melhorar seu jogo. ESTEJA LÁ para eles e eles estarão lá para você. <br /><br />
-    Muitos desenvolvedores mais novos tentarão imediatamente atirar em streamers ou youtubers famosos para alcançar os holofotes. Essas pessoas recebem milhares, senão dezenas de milhares de solicitações todos os dias. Concentre-se em sua comunidade e, eventualmente, um dos grandes nomes poderá notar e divulgar seu produto para seu público. <br /><br />
+    <p>
+        Existem vários lugares online onde você pode postar sobre seus jogos ou o que quer que esteja fazendo. Nunca perca uma oportunidade de fazer isso. Você é um desenvolvedor independente e precisa ficar no topo da montanha e gritar sobre isso. Você não é um Triple A com milhões de dólares para um orçamento de marketing, então aproveite todas as oportunidades que puder para mostrar seu trabalho às pessoas. Seu trabalho não precisa ser perfeito para falar sobre isso, então divulgue-o!
+    </p>
+    <p>
+        A forma como iniciamos esse processo foi configurando contas sociais para ajudar a reunir todas as pessoas em um só lugar. Estabelecemos o Discord como o principal centro comunitário que permitiu às pessoas um lugar comum para se encontrarem e se misturarem. Em seguida, configuramos contas satélite para direcionar as pessoas de volta ao Discord. Isso inclui coisas como <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" >Twitter</a>, <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" >Instagram</a>, <a href="https://store.steampowered.com/" target="_blank" rel="noopener noreferrer" >Steam</a>, <a href="https://gamejolt.com/" target="_blank" rel="noopener noreferrer" >Game Jolt</a>, <a href="https://itch.io/" target="_blank" rel="noopener noreferrer" >Itch.io</a>, <a href="https://twitch.tv" target="_blank" rel="noopener noreferrer" >Twitch</a>, <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" >YouTube</a>, <a href="https://www.reddit.com/" target="_blank" rel="noopener noreferrer" >Reddit</a>, e <a href="https://guilded.gg/" target="_blank" rel="noopener noreferrer" >Guilded</a>. Esse efeito de funil aumentou nosso Discord para mais de 4.000 membros e mantém todos na mesma página para que as atualizações não sejam perdidas pelos jogadores mais engajados.
+    </p>
+    <p>
+        As melhores e mais ativas plataformas de mídia social mudarão com o tempo, com base nos caprichos da Internet. É muito importante estar à frente dessas mudanças ou pelo menos acompanhá-las para não ser pego de surpresa. Eu rotineiramente visito vários sites e crio contas para o Pirate Software para ter certeza de que estamos preparados para o futuro.
+    </p>
+    <p>
+        É importante ver a sua presença nas redes sociais como algo muito pessoal para os seus jogadores. Simplesmente responder postagens aqui e ali, curtir ou entrar no stream de alguém quando ele joga seu jogo é enorme. Pode ser uma coisa simples para você, mas é absolutamente devastador para alguém que ama o seu trabalho. Faça isso. Comente todos os vídeos do YouTube sobre o seu jogo. Responda no Twitter sempre que alguém mencionar seu jogo de forma positiva e converse com as pessoas que responderam negativamente sobre como você pode melhorar seu jogo. ESTEJA LÁ para eles e eles estarão lá para você.
+    </p>
+    <p>
+        Muitos desenvolvedores mais novos tentarão imediatamente atirar em streamers ou youtubers famosos para alcançar os holofotes. Essas pessoas recebem milhares, senão dezenas de milhares de solicitações todos os dias. Concentre-se em sua comunidade e, eventualmente, um dos grandes nomes poderá notar e divulgar seu produto para seu público.
+    </p>
     <div id="nav-marketing-advertising"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Anúncios Pagos</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Geralmente fico longe de qualquer tipo de promoção paga. Como um desenvolvedor independente, você pode não ter muito dinheiro para gastar, então é difícil competir por olhos na internet com tantos grandes jogadores por aí. Tempo e recursos são muito melhor gastos em sua comunidade do que em qualquer outra coisa. <br /><br />
-    Dito isto, você pode querer experimentar diferentes métodos de publicidade. Você pode encontrar algo que funcione para você. Só porque nosso estúdio não encontrou um benefício aqui, não significa que você não encontrará. <br /><br />
+    <p>
+        Geralmente fico longe de qualquer tipo de promoção paga. Como um desenvolvedor independente, você pode não ter muito dinheiro para gastar, então é difícil competir por olhos na internet com tantos grandes jogadores por aí. Tempo e recursos são muito melhor gastos em sua comunidade do que em qualquer outra coisa.
+    </p>
+    <p>
+        Dito isto, você pode querer experimentar diferentes métodos de publicidade. Você pode encontrar algo que funcione para você. Só porque nosso estúdio não encontrou um benefício aqui, não significa que você não encontrará.
+    </p>
     <div id="nav-marketing-conventions"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Convenções</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Definitivamente, participe de convenções, mas certifique-se de não esgotar a carteira do estúdio no processo. É muito fácil esquecer o custo da viagem, alimentação e outras despesas se você não planejá-las com antecedência. Fazer um orçamento de cada viagem antes de partir é importante quando você está apenas começando. <br /><br />
-    Você realmente deveria testar primeiro as águas com convenções locais menores. Nosso primeiro foi a Eugene Comic Con em Eugene Oregon e o estande nos custou apenas 20$ por um fim de semana inteiro. Conhecemos muitas pessoas, ganhamos muitos fãs e aprendemos como administrar melhor nosso estande para futuras convenções. Esse tipo de coisa é inestimável e realmente nos ajudou a chegar lá. <br /><br />
-    A mercadoria pode realmente ajudar a compensar os custos da convenção se você tiver algumas coisas boas. Tente levar adesivos, broches ou pelúcias para esse tipo de show porque eles acabam sendo vendidos muito rápido. Fique longe de itens quebráveis, como canecas, ou itens de tamanho restrito, como roupas, porque é muito mais difícil vender em um ambiente de convenção. <br /><br />
+    <p>
+        Definitivamente, participe de convenções, mas certifique-se de não esgotar a carteira do estúdio no processo. É muito fácil esquecer o custo da viagem, alimentação e outras despesas se você não planejá-las com antecedência. Fazer um orçamento de cada viagem antes de partir é importante quando você está apenas começando.
+    </p>
+    <p>
+        Você realmente deveria testar primeiro as águas com convenções locais menores. Nosso primeiro foi a Eugene Comic Con em Eugene Oregon e o estande nos custou apenas 20$ por um fim de semana inteiro. Conhecemos muitas pessoas, ganhamos muitos fãs e aprendemos como administrar melhor nosso estande para futuras convenções. Esse tipo de coisa é inestimável e realmente nos ajudou a chegar lá.
+    </p>
+    <p>
+        A mercadoria pode realmente ajudar a compensar os custos da convenção se você tiver algumas coisas boas. Tente levar adesivos, broches ou pelúcias para esse tipo de show porque eles acabam sendo vendidos muito rápido. Fique longe de itens quebráveis, como canecas, ou itens de tamanho restrito, como roupas, porque é muito mais difícil vender em um ambiente de convenção.
+    </p>
     <div id="nav-marketing-awards"></div>
-    <br />
     <div class="text-separator"></div>
-    <br />
     <h3>Prêmios</h3>
-    <br />
     <div class="text-separator"></div>
-    <br /><br />
-    Os prêmios não são concedidos apenas a jogos incríveis da indústria. Você precisa solicitá-los e eles quase sempre terão uma taxa de cobertura. O preço geral está entre 50-100$ USD como padrão. Procure os formulários de inscrição para qualquer um dos prêmios dos quais você já ouviu falar e também fique atento a novas premiações menores. <br /><br />
-    Quase sempre há algo acontecendo e um prêmio pode realmente ajudar a colocar você e seu trabalho no mapa. Às vezes, eles também vêm com troféus ou imagens interessantes que você pode vincular às suas páginas públicas de lançamento.
+    <p>
+        Os prêmios não são concedidos apenas a jogos incríveis da indústria. Você precisa solicitá-los e eles quase sempre terão uma taxa de cobertura. O preço geral está entre 50-100$ USD como padrão. Procure os formulários de inscrição para qualquer um dos prêmios dos quais você já ouviu falar e também fique atento a novas premiações menores.
+    </p>
+    <p>
+        Quase sempre há algo acontecendo e um prêmio pode realmente ajudar a colocar você e seu trabalho no mapa. Às vezes, eles também vêm com troféus ou imagens interessantes que você pode vincular às suas páginas públicas de lançamento.
+    </p>
 </div>
 <br /><br />
