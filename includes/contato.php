c2dc169df0396252f416330d40e5f3027bdf6048
Falme Streamless <falme_streamless@hotmail.com>
2024-09-27 22:23:29 -0300

    Modificado Todos os Paragrafos em Contato

--- includes/contato.php
+++ includes/contato.php
@@ -4,14 +4,14 @@
     <img src="assets/images/gamedev_splash.png" style="width: 100%" /> <br />
     <h1>Tem algo que nos ajude a melhorar?</h1>
     <div class="text-separator"></div>
-    <br /><br />
-    Esse site é como um GDD/One Page, sempre recebendo atualizações e melhorando para que todos nós possamos entender e aperfeiçoar nossos projetos. Se por um acaso sentir falta de alguma informação na página, ou caso tenhamos errado alguma parte, nos avise pelo email:
-    <br /><br />
+    <p>
+        Esse site é como um GDD/One Page, sempre recebendo atualizações e melhorando para que todos nós possamos entender e aperfeiçoar nossos projetos. Se por um acaso sentir falta de alguma informação na página, ou caso tenhamos errado alguma parte, nos avise pelo email:
+    </p>
     <p align="center">
         <a href="mailto:contato@crie.games">contato@crie.games</a>
     </p>
-    <br />
-    Podemos demorar um pouquinho para responder seu email, mas eventualmente entraremos em contato.
-    <br /><br />
+    <p>
+        Podemos demorar um pouquinho para responder seu email, mas eventualmente entraremos em contato.
+    </p>
 </div>
 <br /><br />
