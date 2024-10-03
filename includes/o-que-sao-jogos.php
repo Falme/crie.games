a7cf4a19ccdd1f15b8cefcf564472966f406e90d
Falme Streamless <falme_streamless@hotmail.com>
2024-10-03 03:13:54 -0300

    Criado texto inicial do O que são jogos

--- includes/o-que-sao-jogos.php
+++ includes/o-que-sao-jogos.php
@@ -5,7 +5,57 @@
     <h1>O que são Jogos?</h1>
     <div class="text-separator"></div>
     <p>
-        
+        Essa pergunta parece simples, mas essa dúvida vem sendo perguntada há mais de 100 anos. Normalmente só respondemos com <i>"Se eu ver, eu sei o que é"</i>, mas para nós e para vocês criadores não deve ser só isso. Devemos entender o seu conceito e seus limites para que possamos discutir corretamente quando alguém usar jogos para o mal, desinformação ou discurso de ódio.
+    </p>
+    <p>
+        Mas para resumir sua definição, <a href="https://www.jesperjuul.net/text/gameplayerworld/" target="_blank" rel="noopener noreferrer">alguns parâmetros sempre se repetem</a>, desde Johan Huizinga até Jesper Juul. Eles são: 
+    </p>
+    
+    <div class="text-separator"></div>
+
+    <p>
+        <li>
+            <b>Antagônico:</b> Normalmente jogos contém um vencedor e um perdedor, considerando que um empate irá criar uma nova partida até que esse conflito se resolva.
+        </li>
+    </p>
+    <p>
+        <li>
+            <b>Livre:</b> É uma atividade não séria, voluntária, você nunca deve ser obrigado a jogar um jogo.
+        </li>
+    </p>
+    <p>
+        <li>
+            <b>Recreativa:</b> É separada da vida real, representa algo ordinário que se opôe ao trabalho e ofício. Não é algo sério.
+        </li>
+    </p>
+    <p>
+        <li>
+            <b>Incerto:</b> Deve ter um desequilíbrio e assim não saber os resultados antes de jogar o jogo, caso contrário, não há motivos pra jogar se já sabe como irá terminar. 
+        </li>
+    </p>
+    <p>
+        <li>
+            <b>Regrado:</b> Existem regras que devem ser seguidas por todos os participantes, essas não são as regras da vida real, são regras exclusivas do jogo que deve ser respeitado enquanto o jogo acontece.
+        </li>
+    </p>
+    <p>
+        <li>
+            <b>Fictícia:</b> Contém um conflito artificial, jogos criam problemas que não causam nenhum tipo de dano na vida real, os únicos afetados são os personagens não reais do Faz-De-Conta chamado jogo.
+        </li>
+    </p>
+    <p>
+        <li>
+            <b>Improdutivo:</b> Da mesma forma que jogos são fictícios, suas economias, perdas e ganhos também devem ser fictícios, ou seja, não geram bens a sociedade. <a href="https://blogs.correiobraziliense.com.br/capital-sa/2024/09/20/epidemia-das-apostas-brasileiros-ja-gastaram-r-68-bilhoes-em-apostas-on-line/" target="_blank" rel="noopener noreferrer">Sim, isso significa que Loterias e Apostas não são consideradas jogos</a>.
+        </li>
+    </p>
+    
+    <div class="text-separator"></div>
+
+    <p>
+        Também devemos esclarecer que muitas vezes jogos digitais são vistos como uma evolução do jogo de tabuleiro, o que não é verdade, são experiências diferentes em que cada um tem algo único a oferecer.
+    </p>
+    <p>
+        Jogos digitais tem super poderes que faz dele único, como poder jogar o mesmo jogo centenas de vezes com resultados completamente diferentes como em Roguelikes. Histórias emergentes e procedurais complexas como em <a href="https://store.steampowered.com/app/975370/Dwarf_Fortress/" target="_blank" rel="noopener noreferrer">Dwarf Fortress</a> e <a href="https://store.steampowered.com/app/294100/RimWorld/" target="_blank" rel="noopener noreferrer">Rimworld</a>. E mundos inteiros online com centenas ou milhares de jogadores jogando ao mesmo tempo em busca de uma missão ou matar um dragão extremamente poderoso.
     </p>
 </div>
 <br /><br />
