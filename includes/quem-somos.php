739e3f01f3a008a0b27a3ae1ddfb4b0035b67e0e
Falme Streamless <falme_streamless@hotmail.com>
2024-10-22 20:24:11 -0300

    Adicionado mudanças para versão 0.0.4

--- includes/quem-somos.php
+++ includes/quem-somos.php
@@ -10,13 +10,10 @@
         <b><a href="https://kvdon.itch.io/" target="_blank" rel="noopener noreferrer">Danilo Cavedon</a></b>: Game designer e desenvolvedor com experiência em C# para Unity, HTML5, CSS, PHP. Na Hyperactive Desenvolvimento Digital, trabalhou com marketing, identidade visual, advergames e jogos educacionais. Desde 2017, na Ficciona Produções, aplica sua especialização em storytelling e jogos digitais, apoiando o desenvolvimento de novos projetos. Atualmente, trabalha como programador e game designer nos projetos “Playpolis - Brincar de História” e “Super Idade”.
     </p>
     <p>
-        <b><a href="https://falme.com.br/" target="_blank" rel="noopener noreferrer" >Falme Streamless</a></b>: Desenvolvedor de jogos, Desenvolvedor de websites e entusiasta em Segurança da Informação que já mexeu com várias tecnologias como Unity3D C#, Flash ActionScript, Construct2, XNA e outras que não existem mais. Trabalhei com jogos de entretenimento, educacionais, advergames e porting para várias plataformas como Xbox One, PS4, Nintendo Switch, Android, iOS, WebGL e Steam. Também auxilio alunos de faculdade de jogos digitais e mentorias para pequenos grupos e/ou empresas. Atualmente me aventurando na ferramenta Godot Engine.
+        <b><a href="https://falme.com.br/" target="_blank" rel="noopener noreferrer" >Falme Streamless</a></b>: Desenvolvedor de Jogos, Web e entusiasta em Segurança da Informação que já trabalhou com tudo: Game Engines, Frameworks, Porting e Publicação. Hoje trabalho com jogos educacionais e faço mentorias para quem está começando na área.
     </p>
     <p>
         Nos conhecemos na faculdade de Jogos Digitais, fizemos e estamos fazendo vários projetos juntos, mas sentimos a necessidade de retribuir o que a área de jogos nos ofereceu e ajudar novos criadores de jogos a melhorar o mercado e a cultura brasileira.
-    </p>
-    <p>
-        Logo abaixo está várias pequenas recomendações para qualquer um que queira desenvolver jogos. As recomendações não vão funcionar para todos. É muita coisa e muitas vezes será difícil, chato, longo e demorado. Desenvolver jogos é como qualquer outro trabalho. Mas se for isso que está em busca, saiba que este lugar é seu ponto de salvamento ou checkpoint, toda vez que tiver uma dúvida, volte aqui para que tudo fique mais claro. E que seja tão divertido fazer jogos quanto é divertido jogar jogos.
     </p>
     <p>
         E mais uma coisa, nós estamos criando e reproduzindo essas dicas e recursos voltado para o público e contexto brasileiro.
