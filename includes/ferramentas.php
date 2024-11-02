34d1defa2cda386901a5038727a93ce95e712175
Falme Streamless <falme_streamless@hotmail.com>
2024-11-02 15:20:22 -0300

    Adicionado GameIcons ao Repositorios

--- includes/ferramentas.php
+++ includes/ferramentas.php
@@ -119,7 +119,12 @@
     <div class="text-separator"></div>
     <img src="assets/images/opengameart.png" /> <br />
     <p>
-        <a href="https://opengameart.org/" target="_blank" rel="noopener noreferrer">Open Game Art</a> é um clássico para quem está buscando assets de arte, textura ou 3D gratuito para usar em seus jogos. Normalmente o estilo artistico não vai combinar com o seu projeto ou outros assets, mas é perfeito para protótipos e game jams. Lembre-se sempre de colocar o autor nos créditos.
+        <a href="https://opengameart.org/" target="_blank" rel="noopener noreferrer">OpenGameArt.Org</a> é um clássico para quem está buscando assets de arte, textura ou 3D gratuito para usar em seus jogos. Normalmente o estilo artistico não vai combinar com o seu projeto ou outros assets, mas é perfeito para protótipos e game jams. Lembre-se sempre de colocar o autor nos créditos.
+    </p>
+    <div class="text-separator"></div>
+    <img src="assets/images/game-icons-net.png" /> <br />
+    <p>
+        <a href="https://game-icons.net/" target="_blank" rel="noopener noreferrer">Game-icons.net</a> contém icones em SVG feito pela comunidade, tem lindos ícones para ser usado gratuítamente em seu jogo, e se precisar editar, ele já está vetorizado. Só colocar o autor nos créditos, só isso, nada mais.
     </p>
     <div class="text-separator"></div>
     <img src="assets/images/xelu-controller-keyboard-prompts.png" /> <br />
