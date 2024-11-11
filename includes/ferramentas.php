99cf5cb64ff2f15dbc402c1aa47f0d8455d6db1f
Falme Streamless <falme_streamless@hotmail.com>
2024-11-11 20:46:43 -0300

    Corrigido termo de Este para Essa em Ferramentas

--- includes/ferramentas.php
+++ includes/ferramentas.php
@@ -134,7 +134,7 @@
     <div class="text-separator"></div>
     <img src="assets/images/ultimate-resource-database.png" /> <br />
     <p>
-        Se você não encontrou aqui o repositório que precisava, ele vai estar em <a href="https://gentleland.notion.site/Ultimate-Resource-Database-9f7f86e9ce5a408090eee1985182d951" target="_blank" rel="noopener noreferrer">Ultimate Resource Database</a>. Este é uma pagina do Notion com diversos links para diversas ferramentas, ela sendo paga, por assinatura ou gratuita. Dê uma olhada nas listas desse repositório, você pode achar algo melhor do que procura.
+        Se você não encontrou aqui o repositório que precisava, ele vai estar em <a href="https://gentleland.notion.site/Ultimate-Resource-Database-9f7f86e9ce5a408090eee1985182d951" target="_blank" rel="noopener noreferrer">Ultimate Resource Database</a>. Essa é uma pagina do Notion com diversos links para diversas ferramentas, ela sendo paga, por assinatura ou gratuita. Dê uma olhada nas listas desse repositório, você pode achar algo melhor do que procura.
     </p>
 </div>
 <br /><br />
