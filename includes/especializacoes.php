3d22aba60bebae5ded8587c00255fd3a73e8b862
Falme Streamless <falme_streamless@hotmail.com>
2025-08-15 09:20:39 -0300

    corrigido separação de colunas bootstrap

--- includes/especializacoes.php
+++ includes/especializacoes.php
@@ -1,24 +1,35 @@
+<?php
+	
+?>
+
 <div id="especializacoes"></div>
 <br /><br />
 <div class="especializacoesbox ZGizmo">
-	<div class="row m-0">
-		<a href="../pages/repositorios.php" class="col-4 col-lg-12 btn-especializacoes">
-			Repositórios
-		</a>
+	<div class="row justify-content-around">
+		<div class="col-12 col-md-4 p-3">
+			<a href="../pages/repositorios.php" class="btn-especializacoes">
+				Repositórios
+			</a>
+		</div>
+		<div class="col-12 col-md-4 p-3">
+			<a href="../pages/repositorios.php" class="btn-especializacoes">
+				Repositórios
+			</a>
+		</div>
+		<div class="col-12 col-md-4 p-3">
+			<a href="../pages/repositorios.php" class="btn-especializacoes">
+				Repositórios
+			</a>
+		</div>
+		<div class="col-12 col-md-4 p-3">
+			<a href="../pages/repositorios.php" class="btn-especializacoes">
+				Repositórios
+			</a>
+		</div>
+		<div class="col-12 col-md-4 p-3">
+			<a href="../pages/repositorios.php" class="btn-especializacoes">
+				Repositórios
+			</a>
+		</div>
 	</div>
 </div>
-<!-- 
-<div class="textbox">
-    <img src="assets/images/gamedev_splash.png" style="width: 100%" /> <br />
-    <h1>Tem algo que nos ajude a melhorar?</h1>
-    <div class="text-separator"></div>
-    <p>
-        Esse site é como um GDD/One Page, sempre recebendo atualizações e melhorando para que todos nós possamos entender e aperfeiçoar nossos projetos. Se por um acaso sentir falta de alguma informação na página, ou caso tenhamos errado alguma parte, nos avise pelo email:
-    </p>
-    <p align="center">
-    </p>
-    <p>
-        Podemos demorar um pouquinho para responder seu email, mas eventualmente entraremos em contato.
-    </p>
-</div>
--->
