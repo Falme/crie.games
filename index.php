19ebef43365887234f2f62c382961d73b5e1461b
Falme Streamless <falme_streamless@hotmail.com>
2025-08-16 06:36:36 -0300

    corrigido referencias em repositorios.php

--- index.php
+++ index.php
@@ -4,25 +4,9 @@
 
 	<body>
 		<div class="container-fluid p-0 shell">
-			<div id="header" class="row m-0">
-				<div class="col-10">
-					<div class="nav-section" style="margin-left: 2rem; top: 0.5rem; position: relative">
-						<h3 class="header-title"> Crie Games </h3> 
-						<div class="version">
-							v0.0.6 - Edição: Apoie Eventos Locais 
-							<?php href("[1]", "https://www.instagram.com/firmagamedev/"); ?>
-							<?php href("[2]", "https://www.instagram.com/eaijogos/"); ?>
-							<?php href("[3]", "https://www.instagram.com/newgameplusfest/"); ?>
-							<?php href("[4]", "https://www.instagram.com/hugsp_/"); ?>
-							<?php href("[5]", "https://recnplay.pe/"); ?>
-						</div>
-					</div>
-				</div>
-				<div class="col-2">
-					<img id="hamburger" src="assets/images/menu.svg" />
-				</div>
-				<div class="separator ZGizmo"></div>
-			</div>
+			
+			<?php include($_SERVER['DOCUMENT_ROOT'].'/title.php'); ?>
+
 			<div id="lateral-menu" class="fixed-hide nav-section">
 				<ol>
 					<li><?php href("Quem Somos Nós?", "#nav-game"); ?></li>
