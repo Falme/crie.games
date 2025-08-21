1c28377f808b471142624b29c38208122bee7d64
Falme Streamless <falme_streamless@hotmail.com>
2025-08-21 14:30:08 -0300

    adicionado alguns jogos na pagina da unreal

--- pages/programacao/unrealengine.php
+++ pages/programacao/unrealengine.php
@@ -3,8 +3,12 @@
 <p>
     <?php href("Unreal Engine", "https://www.unrealengine.com/en-US/download"); ?> é super poderoso na arena 3D. Possui um ótimo modelo de monetização. Se o seu jogo ganhar 
     <?php href("menos de US$ 1.000.000 ao longo de sua vida útil", "https://www.unrealengine.com/pt-BR/release"); ?>, você não pagará nada. Qualquer coisa além disso tem royalties de 5% marcados. Eles também são totalmente gratuitos para uso em projetos não monetizados. Definitivamente ótimo para startups, mas pode ter uma curva de aprendizado bem difícil e específica. Unreal foi usado para fazer 
-    <?php href("Fortnite", "https://www.epicgames.com/fortnite/en-US/home"); ?>, 
-    <?php href("Conan Exiles", "https://store.steampowered.com/app/440900/Conan_Exiles/"); ?>, 
-    <?php href("Kingdom Hearts 3", "https://www.kingdomhearts.com/3/us/home/"); ?>, e 
-    <?php href("Octopath Traveler", "https://www.nintendo.com/games/detail/octopath-traveler-switch/"); ?>.
+    <?php href("MechaBlade Hero", "https://www.catarse.me/mechablade_hero"); ?>
+    , 
+    <?php href("Avante! Atlantis", "https://store.steampowered.com/app/3172160/Avante_Atlantis/"); ?>
+    , 
+    <?php href("Aquele Caos Até Bugar", "https://gatinhasdiabolicas.itch.io/acab"); ?>
+    , e 
+    <?php href("Wonderbox: The Adventure Maker", "https://br.ign.com/wonderbox-the-adventure-maker/121844/news/jogo-brasileiro-comprado-pela-epic-games-sera-descontinuado-e-nao-estara-mais-disponivel-a-partir-de"); ?>
+    .
 </p>
