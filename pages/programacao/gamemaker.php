76f26390ce42384eba7a152c5667f68a5ca5fae5
Falme Streamless <falme_streamless@hotmail.com>
2025-08-21 14:18:29 -0300

    adicionado tamashii e teocida a pagina gamemaker

--- pages/programacao/gamemaker.php
+++ pages/programacao/gamemaker.php
@@ -4,12 +4,7 @@
     <?php href("GameMaker", "https://www.yoyogames.com/en/gamemaker"); ?>
     é uma ótima engine para fazer jogos 2D, mas não muito para 3D (Mas isso nunca impediu ninguém, 
     jogos como 
-    <?php href("Spooky's House of Jump Scares", "https://store.steampowered.com/app/356670/Spookys_Jump_Scare_Mansion/"); ?> e 
-    <?php href("Buck Up And Drive!", "https://gamemaker.io/pt-BR/blog/making-3d-in-gamemaker-drive"); ?> são muito bem vistos). Você pode usar a avaliação gratuita indefinidamente para uso não comercial e permite exportações para desktop PC, bem como para Web e mobile. O modelo de monetização é a seguinte: 100 dólares uma vez para uma licença vitalícia, para Consoles são 80 dólares por mês ou 800 dólares por ano. Eles não cobram royalties, o que é ótimo se o seu jogo der super certo. O Game Maker Studio foi usado para fazer 
-    <?php href("Heartbound", "https://store.steampowered.com/app/567380/Heartbound/"); ?>, 
-    <?php href("Hyper Light Drifter", "https://store.steampowered.com/app/257850/Hyper_Light_Drifter/"); ?>, 
-    <?php href("Risk of Rain", "https://store.steampowered.com/app/248820/Risk_of_Rain/"); ?>, 
-    <?php href("Nuclear Throne", "https://store.steampowered.com/app/242680/Nuclear_Throne/"); ?>, 
-    <?php href("Pizza Tower", "https://store.steampowered.com/app/2231450/Pizza_Tower/"); ?> e 
-    <?php href("Undertale", "https://store.steampowered.com/app/391540/Undertale/"); ?>. 
+    <?php href("Spooky's House of Jump Scares", "https://store.steampowered.com/app/356670/Spookys_Jump_Scare_Mansion/"); ?> são muito bem vistos). Você pode usar a avaliação gratuita indefinidamente para uso não comercial e permite exportações para desktop PC, bem como para Web e mobile. O modelo de monetização é a seguinte: 100 dólares uma vez para uma licença vitalícia, para Consoles são 80 dólares por mês ou 800 dólares por ano. Eles não cobram royalties, o que é ótimo se o seu jogo der super certo. O Game Maker Studio foi usado para fazer 
+    <?php href("Tamashii", "https://store.steampowered.com/app/1026400/TAMASHII/"); ?> e 
+    <?php href("Teocida", "https://store.steampowered.com/app/1569470/TEOCIDA/"); ?>.
 </p>
