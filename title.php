4e59896648b4ee7083b214de43a7c739ee48fc62
Falme Streamless <falme_streamless@hotmail.com>
2025-08-26 09:44:36 -0300

    mudado versao para 0.0.7

--- title.php
+++ title.php
@@ -3,12 +3,7 @@
 		<div class="nav-section" style="margin-left: 2rem; top: 0.5rem; position: relative">
 			<h3 class="header-title"> <a href="/">Crie Games</a> </h3> 
 			<div class="version">
-				v0.0.6 - Edição: Apoie Eventos Locais 
-				<?php href("[1]", "https://www.instagram.com/firmagamedev/"); ?>
-				<?php href("[2]", "https://www.instagram.com/eaijogos/"); ?>
-				<?php href("[3]", "https://www.instagram.com/newgameplusfest/"); ?>
-				<?php href("[4]", "https://www.instagram.com/hugsp_/"); ?>
-				<?php href("[5]", "https://recnplay.pe/"); ?>
+				v0.0.7 - Edição: Silksong 
 			</div>
 		</div>
 	</div>
