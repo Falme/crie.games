03047767af7a384a820ee13448a8ce14de7b99f3
Falme Streamless <falme_streamless@hotmail.com>
2025-09-01 17:36:24 -0300

    corrigido espaçamento de listas na sessão oficina

--- includes/oficina-links.php
+++ includes/oficina-links.php
@@ -16,64 +16,70 @@
 
 	<?php href("", ""); ?>
 
-    <li><?php href("Homo ludens: O jogo como elemento da cultura", "https://www.amazon.com.br/Homo-Ludens-Johan-Huizinga/dp/8527311577"); ?>
-    <li><?php href("Os jogos e os homens: A máscara e a vertigem", "https://www.amazon.com.br/Os-jogos-homens-m%C3%A1scara-vertigem/dp/8532655254"); ?>
-    <li><?php href("Game Design: Método da Estrela [Curso Online]", "https://hotmart.com/pt-br/marketplace/produtos/game-design-metodo-da-estrela/G58887838Y"); ?>
-    <li><?php href("Regras do Jogo: Fundamentos do Design de Jogos", "https://www.amazon.com.br/Regras-Jogo-Fundamentos-Design-Jogos/dp/8521206267"); ?>
-    <li><?php href("Level Up: Um Guia para o Design de Grandes Jogos", "https://www.amazon.com/Level-Up-Em-Portuguese-Brasil/dp/852120700X"); ?>
-    <li><?php href("Game Design Workshop: A Playcentric Approach to Creating Innovative Games", "https://www.amazon.com/Game-Design-Workshop-Playcentric-Innovative/dp/0240809742"); ?>
-    <li><?php href("A Arte De Game Design. O Livro Original", "https://www.amazon.com.br/Arte-Game-Design-Livro-Original/dp/8535241981"); ?>
-    <li><?php href("Challenges for Games Designers: Non-Digital Exercises for Video Game Designers", "https://www.amazon.com/Challenges-Games-Designers-Non-Digital-Exercises/dp/1542453313"); ?>
+    <li><?php href("Homo ludens: O jogo como elemento da cultura", "https://www.amazon.com.br/Homo-Ludens-Johan-Huizinga/dp/8527311577"); ?></li>
+    <li><?php href("Os jogos e os homens: A máscara e a vertigem", "https://www.amazon.com.br/Os-jogos-homens-m%C3%A1scara-vertigem/dp/8532655254"); ?></li>
+    <li><?php href("Game Design: Método da Estrela [Curso Online]", "https://hotmart.com/pt-br/marketplace/produtos/game-design-metodo-da-estrela/G58887838Y"); ?></li>
+    <li><?php href("Regras do Jogo: Fundamentos do Design de Jogos", "https://www.amazon.com.br/Regras-Jogo-Fundamentos-Design-Jogos/dp/8521206267"); ?></li>
+    <li><?php href("Level Up: Um Guia para o Design de Grandes Jogos", "https://www.amazon.com/Level-Up-Em-Portuguese-Brasil/dp/852120700X"); ?></li>
+    <li><?php href("Game Design Workshop: A Playcentric Approach to Creating Innovative Games", "https://www.amazon.com/Game-Design-Workshop-Playcentric-Innovative/dp/0240809742"); ?></li>
+    <li><?php href("A Arte De Game Design. O Livro Original", "https://www.amazon.com.br/Arte-Game-Design-Livro-Original/dp/8535241981"); ?></li>
+    <li><?php href("Challenges for Games Designers: Non-Digital Exercises for Video Game Designers", "https://www.amazon.com/Challenges-Games-Designers-Non-Digital-Exercises/dp/1542453313"); ?></li>
 
     <div class="text-separator"></div>
     <h3>Videos</h3>
     <div class="text-separator"></div>
 
-    <li><?php href("O Mercado Informal de Video Games no Brasil", "https://www.youtube.com/watch?v=MU29Wqg_BVo"); ?>
-    <li><?php href("1983 - O Ano dos Videogames no Brasil", "https://www.youtube.com/watch?v=BpYfeR7p8yw"); ?>
-    <li><?php href("It's Hard To Make Games", "https://www.youtube.com/watch?v=oE1Rm7F7f20"); ?>
-    <li><?php href("The Four Types of Video Game Designers - Extra Credits", "https://www.youtube.com/watch?v=suhANDk1h40"); ?>
-    <li><?php href("One-Page Designs", "https://gdcvault.com/play/1012356/One-Page"); ?>
-    <li><?php href("Core Game Loops", "https://www.youtube.com/watch?v=aDE69s3pp7M"); ?>
-    <li><?php href("The Real Core Loop - What Every Game Has In Common - Extra Credits", "https://www.youtube.com/watch?v=mGL5YGcAxEI"); ?>
-    <li><?php href("The Skinner Box - How Games Condition People to Play More - Extra Credits", "https://www.youtube.com/watch?v=tWtvrPTbQ_c"); ?>
-    <li><?php href("How Video Game Economies are Designed", "https://www.youtube.com/watch?v=Zrf1cou_yVo"); ?>
-    <li><?php href("The Three Pillars of Game Writing - Plot, Character, Lore - Extra Credits", "https://www.youtube.com/watch?v=wNNXdoj7cCQ"); ?>
-    <li><?php href("A Jornada do Herói Dublado (PT-BR)", "https://www.youtube.com/watch?v=Stdko2NIUNI"); ?>
-    <li><?php href("Dan Harmon's Story Circle | Rick and Morty", "https://www.youtube.com/watch?v=RG4WcRAgm7Y"); ?>
-    <li><?php href("Como Escrever Tramas: como o criador de Rick and Morty cria histórias", "https://www.youtube.com/watch?v=Y2vPPMB7OAs"); ?>
-    <li><?php href("Writing Advice from Matt Stone & Trey Parker @ NYU", "https://www.youtube.com/watch?v=vGUNqq3jVLg"); ?>
-    <li><?php href("Why I'm Credited In Over 100 Games (not even joking)", "https://www.youtube.com/watch?v=d6GtGbI-now"); ?>
-    <li><?php href("So You Want To Be a Producer - How to Lead a Development Team - Extra Credits", "https://www.youtube.com/watch?v=WUkcz2taCiw"); ?>
-    <li><?php href("Why Subnautica Owes Its Success to This Button", "https://www.youtube.com/watch?v=zVnOcmiEdIE"); ?>
-    <li><?php href("Design Club - Super Mario Bros: Level 1-1 - How Super Mario Mastered Level Design", "https://www.youtube.com/watch?v=ZH2wGpEZVgE"); ?>
+    <li><?php href("O Mercado Informal de Video Games no Brasil", "https://www.youtube.com/watch?v=MU29Wqg_BVo"); ?></li>
+    <li><?php href("1983 - O Ano dos Videogames no Brasil", "https://www.youtube.com/watch?v=BpYfeR7p8yw"); ?></li>
+    <li><?php href("It's Hard To Make Games", "https://www.youtube.com/watch?v=oE1Rm7F7f20"); ?></li>
+    <li><?php href("The Four Types of Video Game Designers - Extra Credits", "https://www.youtube.com/watch?v=suhANDk1h40"); ?></li>
+    <li><?php href("One-Page Designs", "https://gdcvault.com/play/1012356/One-Page"); ?></li>
+    <li><?php href("Core Game Loops", "https://www.youtube.com/watch?v=aDE69s3pp7M"); ?></li>
+    <li><?php href("The Real Core Loop - What Every Game Has In Common - Extra Credits", "https://www.youtube.com/watch?v=mGL5YGcAxEI"); ?></li>
+    <li><?php href("The Skinner Box - How Games Condition People to Play More - Extra Credits", "https://www.youtube.com/watch?v=tWtvrPTbQ_c"); ?></li>
+    <li><?php href("How Video Game Economies are Designed", "https://www.youtube.com/watch?v=Zrf1cou_yVo"); ?></li>
+    <li><?php href("The Three Pillars of Game Writing - Plot, Character, Lore - Extra Credits", "https://www.youtube.com/watch?v=wNNXdoj7cCQ"); ?></li>
+    <li><?php href("A Jornada do Herói Dublado (PT-BR)", "https://www.youtube.com/watch?v=Stdko2NIUNI"); ?></li>
+    <li><?php href("Dan Harmon's Story Circle | Rick and Morty", "https://www.youtube.com/watch?v=RG4WcRAgm7Y"); ?></li>
+    <li><?php href("Como Escrever Tramas: como o criador de Rick and Morty cria histórias", "https://www.youtube.com/watch?v=Y2vPPMB7OAs"); ?></li>
+    <li><?php href("Writing Advice from Matt Stone & Trey Parker @ NYU", "https://www.youtube.com/watch?v=vGUNqq3jVLg"); ?></li>
+    <li><?php href("Why I'm Credited In Over 100 Games (not even joking)", "https://www.youtube.com/watch?v=d6GtGbI-now"); ?></li>
+    <li><?php href("So You Want To Be a Producer - How to Lead a Development Team - Extra Credits", "https://www.youtube.com/watch?v=WUkcz2taCiw"); ?></li>
+    <li><?php href("Why Subnautica Owes Its Success to This Button", "https://www.youtube.com/watch?v=zVnOcmiEdIE"); ?></li>
+    <li><?php href("Design Club - Super Mario Bros: Level 1-1 - How Super Mario Mastered Level Design", "https://www.youtube.com/watch?v=ZH2wGpEZVgE"); ?></li>
     
     <div class="text-separator"></div>
     <h3>Ferramentas</h3>
     <div class="text-separator"></div>
     
-    <li><?php href("Godot Engine", "https://godotengine.org/"); ?>
-    <li><?php href("Unreal Engine", "https://www.unrealengine.com/en-US/download"); ?>
-    <li><?php href("Unity3D", "https://unity.com/pt"); ?>
-    <li><?php href("Game Maker", "https://gamemaker.io/en"); ?>
-    <li><?php href("Construct", "https://www.construct.net/en"); ?>
-    <li><?php href("Platformer Toolkit", "https://gmtk.itch.io/platformer-toolkit"); ?>
-    <li><?php href("Spine", "https://pt.esotericsoftware.com/"); ?>
-    <li><?php href("Blender", "https://www.blender.org/download/"); ?>
-    <li><?php href("Davinci Resolver 19", "https://www.blackmagicdesign.com/br/products/davinciresolve"); ?>
-    <li><?php href("Aseprite", "https://www.aseprite.org/"); ?>
-    <li><?php href("Libresprite", "https://libresprite.github.io/#!/"); ?>
-    <li><?php href("Gimp", "https://www.gimp.org/"); ?>
-    <li><?php href("Tenacity", "https://tenacityaudio.org/"); ?>
-    <li><?php href("Ardour", "https://ardour.org/"); ?>
-    <li><?php href("FamiStudio", "https://famistudio.org/"); ?>
-    <li><?php href("AnvilStudio", "https://www.anvilstudio.com/"); ?>
-    <li><?php href("FL Studio", "https://www.image-line.com/fl-studio"); ?>
-    <li><?php href("JSFXR", "https://sfxr.me/"); ?>
-    <li><?php href("FMOD", "https://www.fmod.com/"); ?>
-    <li><?php href("WWise", "https://www.audiokinetic.com/en/wwise/overview/"); ?>
-    <li><?php href("Game UI Database", "https://www.gameuidatabase.com/"); ?>
-    <li><?php href("Interface In Game", "https://interfaceingame.com/"); ?>
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <li><?php href("Gimp", "https://www.gimp.org/"); ?></li>
+            <li><?php href("Tenacity", "https://tenacityaudio.org/"); ?></li>
+            <li><?php href("Ardour", "https://ardour.org/"); ?></li>
+            <li><?php href("FamiStudio", "https://famistudio.org/"); ?></li>
+            <li><?php href("AnvilStudio", "https://www.anvilstudio.com/"); ?></li>
+            <li><?php href("FL Studio", "https://www.image-line.com/fl-studio"); ?></li>
+            <li><?php href("JSFXR", "https://sfxr.me/"); ?></li>
+            <li><?php href("FMOD", "https://www.fmod.com/"); ?></li>
+            <li><?php href("WWise", "https://www.audiokinetic.com/en/wwise/overview/"); ?></li>
+            <li><?php href("Game UI Database", "https://www.gameuidatabase.com/"); ?></li>
+            <li><?php href("Interface In Game", "https://interfaceingame.com/"); ?></li>
+        </div>
+        <div class="col-12 col-md-6">
+            <li><?php href("Godot Engine", "https://godotengine.org/"); ?></li>
+            <li><?php href("Unreal Engine", "https://www.unrealengine.com/en-US/download"); ?></li>
+            <li><?php href("Unity3D", "https://unity.com/pt"); ?></li>
+            <li><?php href("Game Maker", "https://gamemaker.io/en"); ?></li>
+            <li><?php href("Construct", "https://www.construct.net/en"); ?></li>
+            <li><?php href("Platformer Toolkit", "https://gmtk.itch.io/platformer-toolkit"); ?></li>
+            <li><?php href("Spine", "https://pt.esotericsoftware.com/"); ?></li>
+            <li><?php href("Blender", "https://www.blender.org/download/"); ?></li>
+            <li><?php href("Davinci Resolver 19", "https://www.blackmagicdesign.com/br/products/davinciresolve"); ?></li>
+            <li><?php href("Aseprite", "https://www.aseprite.org/"); ?></li>
+            <li><?php href("Libresprite", "https://libresprite.github.io/#!/"); ?></li>
+        </div>
+    </div>
 
     <div class="text-separator"></div>
     <h3>Game Design Document (GDD)</h3>
