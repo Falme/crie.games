d21197bcc4cbe56056f312405e53ec113fb26fe0
Falme Streamless <falme_streamless@hotmail.com>
2025-10-16 03:07:52 -0300

    adicionado librecounter para analytics

--- footer.php
+++ footer.php
@@ -6,8 +6,13 @@
 
 		<div class="separator XGizmo"></div>
 		<br />
-		<div class="footer">Inspirado na UI da <?php href("Godot Engine", "https://godotengine.org/"); ?></div>
+		<div class="footer">
+			Inspirado na UI da <?php href("Godot Engine", "https://godotengine.org/"); ?>.
+			<br />
+			Utilizando <?php href("LibreCounter", "https://librecounter.org/crie.games/show"); ?> como Analytics.
+		</div>
 		<br />
+	<img src="https://librecounter.org/counter.svg" referrerPolicy="unsafe-url" width="0" />
 	</body>
 	<script src="/assets/js/index.js"></script>
 </html>
